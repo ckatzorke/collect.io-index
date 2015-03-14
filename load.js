8ea4e97fbda6b9ec61b1c4f6de292fbf49447758
Christian Katzorke <c.katzorke@web.de>
2015-03-14 13:29:17 +0100

    refactpred load

--- load.js
+++ load.js
@@ -3,19 +3,33 @@ var indexer = require('./index')();
 var apikey = require('./apikey');
 var gb = require('giantbomb-api')(apikey);
 
+var platforms = {
+    ps4: 146,
+    ps3: 35,
+    ps2: 19,
+    ps1: 22,
+    psvita: 129,
+    psp: 18,
+    psnPs3: 88,
+    psnPsvita: 143,
+    psnPsp: 116
+};
 
-console.log('Getting all games for Playstation...');
-gb.games({
-    filter: {
-        platforms: 129
-    },
-    limit: 100,
-    offset: 0,
-    paging: false
-}).then(function (result) {
-    console.log('found ' + result.totalResults + ' games for ps3');
-    indexer.add(result.results);
-}).
-catch(function (error) {
-    console.error('Error during execution chain: ', error, error.stack);
+var update = [platforms.ps2, platforms.psvita];
+update.map(function (platform) {
+    console.log('Loading games for ' + platform);
+    gb.games({
+        filter: {
+            platforms: platform
+        },
+        limit: 100,
+        offset: 0,
+        paging: false
+    }).then(function (result) {
+        console.log('found ' + result.totalResults + ' games for ' + platform);
+        indexer.add(result.results);
+    }).
+    catch(function (error) {
+        console.error('Error during execution chain: ', error, error.stack);
+    });
 });
